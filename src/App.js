faede91d642d742945721b4c2b696b8e130dfc5d
ncb6206 <ncb6206@gmail.com>
2022-07-05 22:59:02 +0900

    22-07-05

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Login from "./routes/Login"
+import Signup from "./routes/Signup"
+import Home from "./routes/Home"
+import Header from "./components/Header"
+import Result from "./routes/Result"
+import Study from "./routes/Study"
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Routes, Route, Switch, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/Signup" element={<Signup />}></Route>
+          <Route path="/Result" element={<Result />}></Route>
+          <Route path="/Study" element={<Study />}></Route>
+        </Routes>
     </div>
   );
 }
