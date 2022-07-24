1bd177ad7997a143e8abc5cae4624fd7b3ba46b4
ncb6206 <ncb6206@gmail.com>
2022-07-24 14:04:15 +0900

    input deletescrap

--- src/routes/Scrap.js
+++ src/routes/Scrap.js
@@ -82,7 +82,9 @@ const cardData = [
 
 export default function Scrap(){
   const useGetData = () => {
+    const [popup, setPopup] = useState({open: false, title: "", message: "", callback: false});
     const [ListScrap, setListScrap] = useState("");
+    const [ScrapId, setScrapId] = useState("");
     const [Imageurl, setImageurl] = useState("");
 
     const getListScrap = async () => {
@@ -90,11 +92,22 @@ export default function Scrap(){
       await axios.get(postUrl)
       .then((response) => {
         setListScrap(response.data);
-        setImageurl(response.data.condition.conditionMedia[0].img)
         console.log(response.data);
         console.log("성공");
       }).catch(function(error){
-        console.log("실패");
+        console.log(error);
+      });
+    }
+
+    const deleteScrap = async () => {
+      const postUrl = `../archive/${ScrapId}/`;
+      await axios.delete(postUrl)
+      .then((response) => {
+        setPopup({open: true, title: "성공!", message: (response.data.message)});
+        setScrapId(null);
+        console.log("삭제 성공");
+      }).catch(function(error){
+        console.log(error);
       });
     }
 
@@ -105,15 +118,21 @@ export default function Scrap(){
     return {
       ListScrap,
       Imageurl,
+      popup,
+      setPopup,
+      deleteScrap,
+      setScrapId,
     }
   }
   
-  const { ListScrap, Imageurl } = useGetData();
+  const { ListScrap, Imageurl, popup, setPopup, deleteScrap, setScrapId } = useGetData();
 
 /*
     return (
       <>
           <div>
+            <Popup open = {popup.open} setPopup = {setPopup} title = {popup.title} message = {popup.message} callback = {popup.callback}/>
+
               <Header />
               <br /><br /><br /><br /><br />
               <Scrapitem>
@@ -127,14 +146,18 @@ export default function Scrap(){
                               <Grid item xl={3} lg={6} sm={12}>
                                   <Card border='dark'>
                                     <Link to={`/infodisease/${scrap.condition.pk}`} style={{ textDecoration: 'none', color:'black'}}>
-                                      <Card.Img variant="top" src={Imageurl}/>
+                                      <Card.Img variant="top" src={scrap.condition.conditionMedia[0].img}/>
                                     </Link>
                                       <Card.Body align='center'>
                                           <Card.Title style={{ fontSize:'30px'}}>{scrap.condition.kr_name}</Card.Title>
                                           <Button
                                             style={{fontSize: "20px", textTransform: "none", padding: "10px 50px" }}
                                             variant="danger" 
-                                            align-item="center">
+                                            align-item="center"
+                                            onClick={ () => {
+                                              setScrapId(scrap.id)
+                                              deleteScrap()
+                                            }>
                                             삭제
                                           </Button>    
                                       </Card.Body>                                                                          
